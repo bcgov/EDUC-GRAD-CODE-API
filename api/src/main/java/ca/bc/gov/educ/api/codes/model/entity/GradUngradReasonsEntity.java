f98281837cb1265c56751fbf3386822191552c17
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2020-11-23 00:09:05 -0800

    ungrad reason endpoint

---
+++ api/src/main/java/ca/bc/gov/educ/api/codes/model/entity/GradUngradReasonsEntity.java
@@ -0,0 +1,39 @@
+package ca.bc.gov.educ.api.codes.model.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Immutable;
+
+import lombok.Data;
+
+@Data
+@Immutable
+@Entity
+@Table(name = "GRAD_UNGRAD_REASONS")
+public class GradUngradReasonsEntity {
+   
+	@Id
+	@Column(name = "CODE", nullable = false)
+    private String code; 
+	
+	@Column(name = "DESCRIPTION", nullable = true)
+    private String description; 
+	
+	@Column(name = "CREATED_BY", nullable = true)
+    private String createdBy;
+	
+	@Column(name = "CREATED_TIMESTAMP", nullable = true)
+    private Date createdTimestamp;
+	
+	@Column(name = "UPDATED_BY", nullable = true)
+    private String updatedBy;
+	
+	@Column(name = "UPDATED_TIMESTAMP", nullable = true)
+    private Date updatedTimestamp;	
+	
+}
---
+++ api/src/main/java/ca/bc/gov/educ/api/codes/model/transformer/GradUngradReasonsTransformer.java
@@ -0,0 +1,49 @@
+package ca.bc.gov.educ.api.codes.model.transformer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import ca.bc.gov.educ.api.codes.model.dto.GradUngradReasons;
+import ca.bc.gov.educ.api.codes.model.entity.GradUngradReasonsEntity;
+
+
+@Component
+public class GradUngradReasonsTransformer {
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    public GradUngradReasons transformToDTO (GradUngradReasonsEntity gradProgramEntity) {
+    	GradUngradReasons gradUngradReasons = modelMapper.map(gradProgramEntity, GradUngradReasons.class);
+        return gradUngradReasons;
+    }
+
+    public GradUngradReasons transformToDTO ( Optional<GradUngradReasonsEntity> gradProgramEntity ) {
+    	GradUngradReasonsEntity cae = new GradUngradReasonsEntity();
+        if (gradProgramEntity.isPresent())
+            cae = gradProgramEntity.get();
+
+        GradUngradReasons gradUngradReasons = modelMapper.map(cae, GradUngradReasons.class);
+        return gradUngradReasons;
+    }
+
+	public List<GradUngradReasons> transformToDTO (Iterable<GradUngradReasonsEntity> gradUngradReasonsEntities ) {
+		List<GradUngradReasons> gradUngradReasonsList = new ArrayList<GradUngradReasons>();
+        for (GradUngradReasonsEntity gradUngradReasonsEntity : gradUngradReasonsEntities) {
+        	GradUngradReasons gradUngradReasons = new GradUngradReasons();
+        	gradUngradReasons = modelMapper.map(gradUngradReasonsEntity, GradUngradReasons.class);            
+        	gradUngradReasonsList.add(gradUngradReasons);
+        }
+        return gradUngradReasonsList;
+    }
+
+    public GradUngradReasonsEntity transformToEntity(GradUngradReasons gradUngradReasons) {
+        GradUngradReasonsEntity gradUngradReasonsEntity = modelMapper.map(gradUngradReasons, GradUngradReasonsEntity.class);
+        return gradUngradReasonsEntity;
+    }
+}
