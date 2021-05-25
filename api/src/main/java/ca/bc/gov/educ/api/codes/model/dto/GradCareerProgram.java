4b006bf73013edba59fee26386ad68c967ef5d5e
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-05-25 12:42:26 -0700

    Pointing career program to TRAX, so removing CUD operations

--- api/src/main/java/ca/bc/gov/educ/api/codes/model/dto/GradCareerProgram.java
+++ api/src/main/java/ca/bc/gov/educ/api/codes/model/dto/GradCareerProgram.java
@@ -3,23 +3,30 @@ package ca.bc.gov.educ.api.codes.model.dto;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper = false)
 @Component
-public class GradCareerProgram extends BaseModel {
+public class GradCareerProgram {
 	
 	private String code; 
 	private String description; 
 	private String startDate; 
 	private String endDate;
 	
+	public String getCode() {
+    	return code != null ? code.trim():null;
+    }
+	
+	public String getDescription() {
+		return description != null ? description.trim(): null;
+	}
+	
 	@Override
 	public String toString() {
 		return "GradCareerProgram [code=" + code + ", description=" + description + ", startDate=" + startDate
 				+ ", endDate=" + endDate + "]";
-	}	
+	}
+	
 	
 	
 }
